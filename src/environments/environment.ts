6f3c62a1d4734807f57a540d4c3d4088cf41aa62
JessyWts <jamz971@gmail.com>
2026-01-18 15:02:45 +0100

    Update environment configuration to use environment variables and add .env file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  apiUrl: 'https://v1.formula-1.api-sports.io',
-  apiHeader: 'x-apisports-key',
-  apiKey: '5a1dcf18e5f5967c5dad62db60318d14',
+  apiUrl: process.env['API_URL'],
+  apiHeader: process.env['API_HEADER'],
+  apiKey: process.env['API_KEY'],
   production: false,
   version: '1.0.0',
 };
